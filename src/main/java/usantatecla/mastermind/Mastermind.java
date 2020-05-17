e3d085d54d5f28c18ec03af6b486d13ccc7b172c
Steven <hspichardo@gmail.com>
2020-05-17 04:02:14 +0200

    add mvp.pm.-proxy version of mastermind

--- src/main/java/usantatecla/mastermind/Mastermind.java
+++ src/main/java/usantatecla/mastermind/Mastermind.java
@@ -1,56 +1,34 @@
 package usantatecla.mastermind;
 
-import usantatecla.utils.WithConsoleModel;
-import usantatecla.utils.YesNoDialog;
+import usantatecla.mastermind.controllers.AcceptorController;
+import usantatecla.mastermind.controllers.Logic;
+import usantatecla.mastermind.views.View;
 
-public class Mastermind extends WithConsoleModel {
-
-	private Game game;
-
-	private Mastermind() {
-		this.game = new Game();
+public abstract class Mastermind {
+	
+	private Logic logic;
+	
+	private View view;
+	
+	protected Mastermind() {
+		this.logic = new Logic(this.isStandalone());
+		this.view = new View();
 	}
 
-	public void play() {
-		boolean resume;
+	protected abstract Boolean isStandalone();
+
+	protected void play() {
+		AcceptorController acceptorController;
 		do {
-			this.console.writeln(Message.TITLE.getMessage());
-			this.game.getSecretCombination().writeln();
-			boolean finished = false;
-			do {
-				int error;
-				do {
-					int[] codes = new ProposedCombination().read();
-					error = this.game.proposeCombination(codes);
-					if (error != Game.NO_ERROR) {
-						this.console.writeln(Error.values()[error].getMessage());
-					}
-				} while (error != Game.NO_ERROR);
-				this.console.writeln();
-				this.console.writeln(Message.TURN.getMessage().replaceFirst("#turn", "" + game.getTurn()));
-				this.game.getSecretCombination().writeln();
-				int[][][] allCodes = this.game.getCodes();
-				for (int i = 0; i < allCodes.length; i++) {
-					new ProposedCombination().write(allCodes[i][0]);
-					new Result(allCodes[i][1][0], allCodes[i][1][1]).writeln();
-				}
-				if (this.game.isWinner()) {
-					this.console.writeln(Message.WINNER.getMessage());
-					finished = true;
-				} else if (this.game.isLooser()) {
-					this.console.writeln(Message.LOOSER.getMessage());
-					finished = true;
-				}
-			} while (!finished);
-			resume = new YesNoDialog().read(Message.RESUME.getMessage());
-			if (resume) {
-				this.game.clear();
+			acceptorController = this.logic.getController();
+			if (acceptorController != null){
+				this.view.interact(acceptorController);
 			}
-		} while (resume);
+		} while (acceptorController != null); 
+		if (!this.isStandalone()) {
+			this.logic.close();
+		}
 	}
-
-	public static void main(String[] args) {
-		new Mastermind().play();
-	}
-
+	
 }
+
